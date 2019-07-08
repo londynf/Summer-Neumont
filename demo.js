47ac55f4fc78b01eb3df24b5dbc180f1912aa9de
Londyn Fletcher <22lfletcher@go.dsdmail.net>
2019-07-08 17:54:06 -0600

    Games

---
+++ demo.js
@@ -0,0 +1,15 @@
+
+
+
+let yourName = "Tim";
+console.log(yourName);
+
+function sayYourName (name) {
+    console.log("Hello " + name + ", how are you doing?");
+    console.log("Hello " + yourName + ", how are you doing?");
+}
+
+//console.log(name);
+sayYourName("Bob");
+sayYourName("Sally");
+name("Jerry")
